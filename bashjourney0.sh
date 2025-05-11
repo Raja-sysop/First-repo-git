e59e40c11bffbc0d62ef7807441ba59a42666bde
Raja-sysop <alfattahrajasa@gmail.com>
2025-05-11 22:50:51 +0700

    edit bash scripting file

--- bashjourney0.sh
+++ bashjourney0.sh
@@ -13,3 +13,15 @@ echo "i will doing well"
 sleep 1
 
 echo "ok"
+
+# try edit this file in linux and push it on github
+
+echo "test"
+
+sleep 1
+
+echo "bash sccripting"
+
+sleep 1
+
+echo "edit succesfull"

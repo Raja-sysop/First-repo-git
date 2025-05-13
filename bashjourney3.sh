1cfd46bc6730c5d84438fca666fa12da9c335a53
Raja-sysop <alfattahrajasa@gmail.com>
2025-05-13 19:09:29 +0700

    job and cronjob

---
+++ bashjourney3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# learning job and cronjob in bash scripting
+
+# job
+
+x=job_result.log
+
+echo "this code is succesfully run at $(date)" >> $x
+
+# to running the code just use command
+# echo "./namefile" | at 12:00
+
+# cronjob
+
+y=cronjob_result.log
+
+/usr/bin/echo "this code is succesfully run at $(/usr/bin/date)" >> $y
+
+# and we also need to edit crontab -e
+# to setting time 
+# 05 12 * * * /bin/bash /home/raja/github1/first-repo-git/bashjourney3.sh

8330e3053b077ab824110bbd259191e984a577b2
Raja-sysop <alfattahrajasa@gmail.com>
2025-05-15 14:38:04 +0700

    update script

---
+++ bashjourney5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# making a simple update script
+
+echo "...."
+sudo apt update
+
+echo "...."
+sudo apt upgrade
+
+echo "...."
+sudo apt dist-upgrade
+
+echo "...."
+sudo apt autoremove
+sudo apt autoclean
